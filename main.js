a1c4c69b548e1c27e9ea3fde7423040a0fda3e98
Olivier-Kango <olivierkango@gmail.com>
2022-09-14 18:10:38 -0700

    Add the first projects into Javascript

--- main.js
+++ main.js
@@ -73,3 +73,77 @@ const listProjects = [
     },
   },
 ];
+
+
+
+listProjects.forEach((project, index) => {
+ 
+  const {name, Image, experience, description, skills, feature, listPopupProjects} = project;
+ 
+
+const projects = document.querySelector('.projects');
+const section = document.createElement('section');
+section.setAttribute('class', 'works card-1 grid');
+projects.appendChild(section);
+
+const card = document.createElement('div');
+card.setAttribute('class', 'card-container-1');
+card.innerHTML = `<img class="img-version-mobile" src="././img/Snapshoot-Portfolio-1.png" alt="Snapshot Portfolio Image">`
+card.innerHTML = `<img class="img-version-desktop" src="././img/img-desktop-1.png" alt="Snapshot Portfolio Image">`
+section.appendChild(card);
+
+const containerDesktop = document.createElement('div');
+containerDesktop.setAttribute('class', 'grid-container-desktop');
+section.appendChild(containerDesktop);
+
+const title = document.createElement('div');
+containerDesktop.appendChild(title);
+
+const mobileTitle = document.createElement('h1');
+mobileTitle.innerText = name;
+title.appendChild(mobileTitle);
+
+const DeskTitle = document.createElement('h1');
+title.appendChild(DeskTitle);
+
+const paragraph = document.createElement('div');
+containerDesktop.appendChild(paragraph);
+
+const languages = document.createElement('div');
+containerDesktop.appendChild(languages);
+
+const button = document.createElement('div');
+containerDesktop.appendChild(button);
+
+});
+
+
+// <div class="grid-container-desktop">
+//     <div class="title experience">
+//         <h1 class="mobile-title">Tonic</h1>
+//         <h1 class="desktop-title">Tonic</h1>
+//         <div class="container">
+//             <h2>CANOPY</h2>
+//             <ul class="flex-container-list">
+//                 <li>Back End Dev</li>
+//                 <li>2015</li>
+//             </ul>
+//         </div>
+//     </div>
+//     <div class="paragraph">
+//         <p class="mobile-para">A daily selection of privately personalized reads; no accounts <br> or
+//             sign-ups required.</p>
+//     </div>
+//     <div class="languages">
+//         <ul class="flex-lang">
+//             <li><span>html</span></li>
+//             <li><span>css</span></li>
+//             <li><span>javaScript</span></li>
+//         </ul>
+//     </div>
+//     <div class="btn-see-projects">
+//         <a href="#" class="button" id="btn">See Project</a>
+//     </div>
+// </div>
+
+// </section>
