d2564d48ac01362747993c7b143cf563a8650475
Abdo Nejjari <mr.nejjari.abdelhafid@gmail.com>
2022-09-15 17:43:09 +0100

    Add storage.js file

--- storage.js
+++ storage.js
@@ -19,13 +19,3 @@ const setInputValues = (id, value) => {
   const selectInput = document.getElementById(id);
   selectInput.value = value;
 };
-
-const getJson = localStorage.getItem('a');
-const parseJson = JSON.parse(getJson);
-Object.keys(parseJson || {}).forEach((key) => {
-  setInputValues(formStorage.elements[key].name, parseJson[key]);
-});
-
-formStorage.addEventListener('change', () => {
-  intoLocalStorage(nameStorage, emailStorage, messageStorage);
-});
